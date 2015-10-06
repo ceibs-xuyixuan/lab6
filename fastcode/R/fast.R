0f70f6e5c70bf47d48c385a4926eb414f367bddd
xuyixuan <xuyixuan@users.noreply.github.com>
2015-10-06 13:30:24 +0200

    Update fast.R

--- fastcode/R/fast.R
+++ fastcode/R/fast.R
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-knapsack_brute_force <- function(x, W){
-  stopifnot(names(x) == c("w","v")&& is.data.frame(x))
-  n <- nrow(x)
-  i <- 1
-  a <- matrix(as.integer(intToBits(1:(2^n-1))), nrow = 32)
-  sum_wv<- NULL
-  elem <- NULL
-  ele <- NULL
-  for(i in 1 : (2^n-1)){
-    sum_wv$w[i] <- sum(x$w[(which(a[,i] == 1))])  #sum(w)
-    sum_wv$v[i] <- sum(x$v[(which(a[,i] == 1))])  #sum(v)
-    elem[[i]] <- list(which(a[,i]  == 1))
-  }
-  kna <- max(sum_wv$v[which(sum_wv$w <= W)])
-  ele <- elem[[which(kna == sum_wv$v)]][[1]]
-  return(list(value = kna, elements = ele))
-=======
 knapsack_brute_force <- function(x, W, parallel = FALSE){
       stopifnot(names(x) == c("w","v")&& is.data.frame(x))
       n <- nrow(x)
@@ -40,5 +22,4 @@ knapsack_brute_force <- function(x, W, parallel = FALSE){
             ele <- elem[[which(kna == sum_wv$v)]]
       }
       return(list(value = kna, elements = ele))
->>>>>>> 5eb0f9834c93472a7b6491630c0e33dc633c1834
 }
