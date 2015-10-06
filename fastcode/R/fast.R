9a5053d072c1855a66e6c169c599da668c108c84
xuyixuan <xuyixuan@users.noreply.github.com>
2015-10-06 13:36:45 +0200

    Update fast.R

--- fastcode/R/fast.R
+++ fastcode/R/fast.R
@@ -1,25 +1,23 @@
 knapsack_brute_force <- function(x, W, parallel = FALSE){
-      stopifnot(names(x) == c("w","v")&& is.data.frame(x))
-      n <- nrow(x)
-      i <- 1
-      a <- matrix(as.integer(intToBits(1:(2^n-1))), nrow = 32)
-      sum_wv<- NULL
-      elem <- NULL
-      ele <- NULL
-      kna <- NULL
-      if(parallel == FALSE){
-            sum_wv$w <- unlist(lapply(1: (2^n-1),  FUN=function(i){return(sum(x$w[(which(a[,i] == 1))]))}))
-            sum_wv$v <- unlist(lapply(1: (2^n-1),  FUN=function(i){return(sum(x$v[(which(a[,i] == 1))]))}))
-            elem<- lapply(1:(2^n-1), FUN=function(i){which(a[,i]  == 1)})
-            kna <- max(sum_wv$v[which(sum_wv$w <= W)])
-            ele <- elem[[which(kna == sum_wv$v)]]
-      }else{ 
-            cl <- detectCores(logical=FALSE)
-            sum_wv$w <- unlist(mclapply(1: (2^n-1), FUN=function(i){return(sum(x$w[(which(a[,i] == 1))]))}, mc.cores = cl))
-            sum_wv$v <- unlist(mclapply(1: (2^n-1), FUN=function(i){return(sum(x$v[(which(a[,i] == 1))]))}, mc.cores = cl))
-            elem<- mclapply(1:(2^n-1), FUN=function(i){which(a[,i]  == 1)},mc.cores = cl)
-            kna <- max(sum_wv$v[which(sum_wv$w <= W)])
-            ele <- elem[[which(kna == sum_wv$v)]]
-      }
-      return(list(value = kna, elements = ele))
+  stopifnot(names(x) == c("w","v")&& is.data.frame(x))
+  n <- nrow(x)
+  i <- 1
+  a <- matrix(as.integer(intToBits(1:(2^n-1))), nrow = 32)
+  sum_wv<- NULL
+  elem <- NULL
+  ele <- NULL
+  kna <- NULL
+  if(parallel == FALSE){
+    sum_wv$w <- unlist(lapply(1: (2^n-1),  FUN=function(i){return(sum(x$w[(which(a[,i] == 1))]))}))
+    sum_wv$v <- unlist(lapply(1: (2^n-1),  FUN=function(i){return(sum(x$v[(which(a[,i] == 1))]))}))
+    elem<- lapply(1:(2^n-1), FUN=function(i){which(a[,i]  == 1)})
+  }else{ 
+    cl <- detectCores(logical=FALSE)
+    sum_wv$w <- unlist(mclapply(1: (2^n-1), FUN=function(i){return(sum(x$w[(which(a[,i] == 1))]))}, mc.cores = cl))
+    sum_wv$v <- unlist(mclapply(1: (2^n-1), FUN=function(i){return(sum(x$v[(which(a[,i] == 1))]))}, mc.cores = cl))
+    elem<- mclapply(1:(2^n-1), FUN=function(i){which(a[,i]  == 1)},mc.cores = cl)
+  }
+  kna <- max(sum_wv$v[which(sum_wv$w <= W)])
+  ele <- elem[[which(kna == sum_wv$v)]]
+  return(list(value = kna, elements = ele))
 }
